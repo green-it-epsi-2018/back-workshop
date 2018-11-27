e8f7d5ec8b1a93479f7292e1e198221afeb25a69
Tshili <Tshili@mbp-de-tshili.home>
2018-11-28 00:02:14 +0100

    database

--- webservices/services.js
+++ webservices/services.js
@@ -1,27 +1,33 @@
 const express = require('express');
-const dbConnector = "Hello world";
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require('../database/db.js');
 
 
-app.get('/data/building',(req, res)=> {
-    res.send(getEventList());
+app.get('/data/building', (req, res) => {
+    // db.getAll((a) => {
+    //     res.send(a)
+    // });
+
+    db.getAll().then((data) =>
+        res.send(data)).catch(err => console.log("eeeeeerrrrr : " + err));
+
+
+    //res.send();
 });
 
-app.get('/data/level', (req, res)=> {
-    res.send(getEventList());
+app.get('/data/level/:levelNumber', (req, res) => {
+    console.log("the req is : " + req.params.levelNumber);
+    db.getByLevel(req.params.levelNumber).then(data => res.send(data)).catch(err => console.log(err));
+
 });
 
-app.get('/data/room', (req, res) => {
-    res.send(getEventList());
+app.get('/data/room/:roomNumber', (req, res) => {
+
+    db.getByRoom(req.params.roomNumber).then((data) => res.send(data)).catch((err) => console.log("errr room function : " + err));
+
 });
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('app is listening');
 });
-
-function getEventList() {
-    return dbConnector;
-}
-
-
