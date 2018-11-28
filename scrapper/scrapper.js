0e275b9d9c4890c085caea7d26d370c9aabedaf1
Tshili <Tshili@MacBook-Pro-de-Tshili.local>
2018-11-28 14:55:45 +0100

    rdfghjk

--- scrapper/scrapper.js
+++ scrapper/scrapper.js
@@ -1,11 +1,11 @@
 /*
-*	CONFIG
-*/
+ *	CONFIG
+ */
 require('dotenv').config()
 const db = require('../database/db.js')
 /*
-*	MODULE LOADING
-*/
+ *	MODULE LOADING
+ */
 const fs = require('fs')
 const axios = require('axios')
 const regexp = new RegExp('^N?(\\d+[a-zA-Z]*)');
@@ -13,42 +13,42 @@ const regexp = new RegExp('^N?(\\d+[a-zA-Z]*)');
 const $ = require('cheerio')
 
 /*
-*	CONSTANTS CONFIGURATION
-*/
+ *	CONSTANTS CONFIGURATION
+ */
 const PAGE_URL = "http://edtmobilite.wigorservices.net/WebPsDyn.aspx?Action=posETUD&serverid=h&tel=${username}&date=${date}%208:00"
 
-setInterval(main,8000);
+setInterval(main, 8000);
 
-function main(){
-    writeToDb(getUsernameFromCsv().map((user) =>{
+function main() {
+    writeToDb(getUsernameFromCsv().map((user) => {
         return getCalendarForUser(user);
     }));
 }
 
-function getUsernameFromCsv(){
+function getUsernameFromCsv() {
     return JSON.parse(fs.readFileSync('./scrapper/username.json'))
 }
 
 
-function getLines(elements,promo,date){
+function getLines(elements, promo, date) {
     let arr = [];
     let line;
-    $('.Ligne',elements).each((index,element)=>{
+    $('.Ligne', elements).each((index, element) => {
         line = $(element).html();
         arr.push({
-            startDate:getTimestamp($('.Debut',line).text(),date),
-            endDate:getTimestamp($('.Fin',line).text(),date),
-            matiere:$('.Matiere',line).text(),
-            salle:regexp.exec($('.Salle',line).text())[1],
-            prof:$('.Prof',line).text(),
-            promo:promo
+            startDate: getTimestamp($('.Debut', line).text(), date),
+            endDate: getTimestamp($('.Fin', line).text(), date),
+            matiere: $('.Matiere', line).text(),
+            salle: regexp.exec($('.Salle', line).text())[1],
+            prof: $('.Prof', line).text(),
+            promo: promo
         });
     });
     return arr;
 }
 
 
-function getTimestamp(hour,dateString){
+function getTimestamp(hour, dateString) {
     let hourArray = hour.split(':');
     let date = new Date(dateString);
     date.setHours(hourArray[0])
@@ -56,47 +56,48 @@ function getTimestamp(hour,dateString){
     return date.getTime();
 }
 
-function getCalendarForUser(user){
-    return Promise.all(getUrlsWithUser(user.username).map((result)=>{
-        return axios.get(result.url).then((response)=>{
-            return getLines(response.data,user.promo,result.date);
+function getCalendarForUser(user) {
+    return Promise.all(getUrlsWithUser(user.username).map((result) => {
+        return axios.get(result.url).then((response) => {
+            return getLines(response.data, user.promo, result.date);
         });
-    })).then((res)=>{
+    })).then((res) => {
         return flatArray(res);
     });
 }
 
-function getUrlsWithUser(user){
+function getUrlsWithUser(user) {
     let urls = [];
-    let urlForUser = PAGE_URL.replace('${username}',user);
+    let urlForUser = PAGE_URL.replace('${username}', user);
     let date = new Date();
     let resultDate;
     let urlToAdd;
-    for(let i =0;i<5;i++){
-        date.setDate(date.getDate()+1);
-        resultDate = (date.getMonth()+1) +'/'+date.getDate() +'/'+  date.getFullYear();
-        urlToAdd = urlForUser.replace('${date}',resultDate);
-        urls.push({url:urlToAdd,date:resultDate});
+    for (let i = 0; i < 5; i++) {
+        date.setDate(date.getDate() + 1);
+        resultDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+        urlToAdd = urlForUser.replace('${date}', resultDate);
+        urls.push({
+            url: urlToAdd,
+            date: resultDate
+        });
     }
-    return(urls);
+    return (urls);
 }
 
-function writeToDb(documents){
+function writeToDb(documents) {
     Promise.all(documents).then((docs) => {
-        flatArray(docs).forEach((doc)=>{
-            console.log(doc);
-            db.insert(doc);
-            db.getAll().then((result)=>{
-                //console.log(result);
-            });
+        flatArray(docs).forEach((doc) => {
+
+            db.insert(doc.startDate, doc.endDate, doc.matiere, doc.salle, doc.prof, doc.promo);
+
         });
 
     });
 }
 
 
-function flatArray(array){
-    return array.reduce((arr,element)=>{
-        return  arr.concat(element);
-    },[])
+function flatArray(array) {
+    return array.reduce((arr, element) => {
+        return arr.concat(element);
+    }, [])
 }
